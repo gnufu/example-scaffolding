e9ca5bf49c7f5042bd6398e96fc1e57ff8bb5ed0
Harald Steinlechner <hs@vrvis.at>
2016-01-11 19:15:08 +0100

    initial setup

---
+++ build.fsx
@@ -0,0 +1,18 @@
+#I @"packages/build/FAKE/tools/"
+#r @"FakeLib.dll"
+#load "packages/build/SourceLink.Fake/tools/SourceLink.fsx"
+#r "build/vrvis/Aardvark.Fake/bin/Aardvark.Fake.dll"
+
+
+open Fake
+open System
+open System.IO
+open System.Diagnostics
+open Aardvark.Fake
+
+do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+//DefaultTargets.install ["src/Aardvark.Rendering.sln"]
+
+// start build
+RunTargetOrDefault "Default"
